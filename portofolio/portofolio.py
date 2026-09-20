6100c0b26fc55348825eefa916ea9ea20400b864
KEVIN MORENO <kevin.dev.dod@gmail.com>
2026-09-19 22:12:18 -0500

    .

--- portofolio/portofolio.py
+++ portofolio/portofolio.py
@@ -31,8 +31,8 @@ def index() -> rx.Component:
     return rx.box(
         navbar(),
         rx.box(profile(), id="profile", style=section_style),
-        rx.box(tecnologies(), id="technologies", style=section_style),
-        rx.box(proyects(), id="projects", style=section_style),
+        rx.box(technologies(), id="technologies", style=section_style),
+        rx.box(projects(), id="projects", style=section_style),
         rx.box(experience(), id="experience", style=section_style),
         rx.box(studies(), id="studies", style=section_style),
         rx.box(contact(), id="contact", style=section_style),        
